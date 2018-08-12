e945cb8289909c79f34ccf8bbf5b3223c1fd79ba
Mike Kolk <mkoko49@hotmail.com>
2018-08-12 15:20:37 -0500

    updates for encryption algo on pass4SymmKey

--- splunkencrypt.py
+++ splunkencrypt.py
@@ -5,6 +5,62 @@ import base64
 import sys
 
 
+def encrypt(plaintext,splunk_secret):
+    
+    rc4key = splunk_secret[0:16]
+    pwd = plaintext + '\0'
+    ciphertext = rc4(pwd,rc4key)
+    
+    return '$1$' + ciphertext.encode('base64').strip()
+    
+    
+
+def decrypt(ciphertext,splunk_secret):
+
+    rc4key = splunk_secret[0:16]
+    plaintext = rc4(ciphertext[3:].decode('base64'),rc4key).strip('\0')
+    
+    return plaintext
+
+
+def encrypt_pass4SymmKey(plaintext,splunk_secret):
+    
+    rc4key = splunk_secret[0:16]
+    
+    xorkey = 'DEFAULTSA'
+    xorkey = (xorkey*(len(plaintext)/9 + 1))[0:len(plaintext)+1]
+    pwd = [x[0] ^ x[1] for x in zip(unpack(plaintext),unpack(xorkey))]
+    pwd.append(0)
+    pwd = pack(pwd)
+    
+    ciphertext = rc4(pwd,rc4key)
+    
+    return '$1$' + ciphertext.encode('base64').strip()
+    
+    
+
+def decrypt_pass4SymmKey(ciphertext,splunk_secret):
+
+    rc4key = splunk_secret[0:16]
+    
+    plaintext = rc4(ciphertext[3:].decode('base64'),rc4key)
+    #.strip('\0')
+    
+    xorkey = 'DEFAULTSA'
+    xorkey = (xorkey*(len(plaintext)/9 + 1))[0:len(plaintext)-1]
+    plaintext = [x[0] ^ x[1] for x in zip(unpack(plaintext),unpack(xorkey))]
+    plaintext = pack(plaintext)
+    
+    return plaintext
+
+
+
+def unpack(text):
+    return [ord(c) for c in text]
+    
+def pack(list):
+    return ''.join([chr(i) for i in list])
+
 def rc4(data, key):
     """RC4 encryption and decryption method."""
     S, j, out = list(range(256)), 0, []
@@ -21,43 +77,3 @@ def rc4(data, key):
         out.append(chr(ord(ch) ^ S[(S[i] + S[j]) % 256]))
 
     return "".join(out)
-
-
-def encrypt(plaintext,splunk_secret):
-    
-    rc4key = splunk_secret[0:16]
-    
-    #xorkey = 'DEFAULTSA'
-    #xorkey = (xorkey*(len(plaintext)/9 + 1))[0:len(plaintext)]
-    
-    #pwd = [x[0] ^ x[1] for x in zip(unpack(plaintext),unpack(xorkey))]
-    pwd = unpack(plaintext)
-    pwd.append(0)
-    pwd = pack(pwd)
-    
-    ciphertext = rc4(pwd,rc4key)
-    
-    return '$1$' + ciphertext.encode('base64').strip()
-    
-    
-
-def decrypt(ciphertext,splunk_secret):
-
-    rc4key = splunk_secret[0:16]
-    
-    plaintext = rc4(ciphertext[3:].decode('base64'),rc4key).strip('\0')
-    
-    #xorkey = 'DEFAULTSA'
-    #xorkey = (xorkey*(len(plaintext)/9 + 1))[0:len(plaintext)-1]
-    
-    #plaintext = [x[0] ^ x[1] for x in zip(unpack(plaintext),unpack(xorkey))]
-    #plaintext = pack(plaintext)
-    
-    return plaintext
-
-
-def unpack(text):
-    return [ord(c) for c in text]
-    
-def pack(list):
-    return ''.join([chr(i) for i in list])
